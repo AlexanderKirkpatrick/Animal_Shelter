747e872bf1f1356c27fe450337af3afaf3f09f88
Alexander <knibz@protonmail.com>
2022-07-20 22:26:44 -0400

    add initial files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Animal_Shelter.Models;
+
+namespace Animal_Shelter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+      
+    }
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Animal_Shelter.Models
+{
+     public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Description { get; set; }
+  }
+}
